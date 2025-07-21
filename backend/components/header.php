36a7c1ed82e2690555548d4da16fbe40129c0969
Polpaing <kizzednx@gmail.com>
2025-07-21 14:54:56 +0700

    Delete backend directory

--- backend/components/header.php
+++
@@ -1,8 +0,0 @@
-<nav class="bg-dark text-white p-3 vh-100">
-    <h4 class="text-center mb-4">Admin Page</h4>
-    <ul class="nav flex-column"> 
-        <li class="nav-item"><a href="../backend/index.php" class="nav-link text-white">Dashbard</a></li>
-        <li class="nav-item"><a href="../backend/user.php" class="nav-link text-white">จัดการผู้ใช้งาน</a></li>
-        <li class="nav-item"><a href="../backend/product.php" class="nav-link text-white">รายการอาหาร</a></li>
-    </ul>
-</nav>
