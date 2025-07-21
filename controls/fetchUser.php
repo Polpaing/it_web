26d8d6305a6e0e9821ab226faec5b8dad0ca19f2
Polpaing <kizzednx@gmail.com>
2025-07-21 14:55:47 +0700

    Delete controls directory

--- controls/fetchUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'db.php';
-
-
-    // ดึงข้อมูลผู้ใช้งานจาก Data base
-    $sql = "SELECT `id`, `first_name`, `last_name`, `address`, `phone`, `email`, `password`, `created_at`, `role`, `profile_image` FROM `users`";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-?>
